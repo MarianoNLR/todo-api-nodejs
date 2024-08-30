d45f24cd03eb23d1e8824d4e843413118fef7442
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-30 18:33:34 -0300

    + getbyid

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { authUser } from '../middlewares/authUser.js'
-import {getMyTasks, addTask, update } from '../controllers/taskController.js'
+import {getMyTasks, addTask, update, getTaskById } from '../controllers/taskController.js'
 
 const taskRouter = express.Router()
 
 taskRouter.get('/', authUser, getMyTasks)
+taskRouter.get('/:taskId', authUser, getTaskById)
 taskRouter.post('/', authUser, addTask)
 taskRouter.put('/:taskId', authUser, update)
 
