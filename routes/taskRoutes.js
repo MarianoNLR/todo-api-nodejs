1c7176bccdf48e48d6eab6f46da5758de8947749
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-08-31 17:49:12 -0300

    add subtask endpoint

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { authUser } from '../middlewares/authUser.js'
-import {getMyTasks, addTask, update, getTaskById } from '../controllers/taskController.js'
+import {getMyTasks, addTask, update, getTaskById, newSubtask } from '../controllers/taskController.js'
 
 const taskRouter = express.Router()
 
+taskRouter.patch('/:taskId/newSubtask', authUser, newSubtask)
 taskRouter.get('/', authUser, getMyTasks)
 taskRouter.get('/:taskId', authUser, getTaskById)
 taskRouter.post('/', authUser, addTask)
